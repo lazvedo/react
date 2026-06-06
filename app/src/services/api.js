71cfc69bb9190d444c0a461372bb499493f9ae23
Luquitas <azevedolucas8002@gmail.com>
2026-06-06 13:02:57 +0000

    atualizacao

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_KEY = import.meta.env.VITE_TMDB_KEY
+const API_KEY = '31dbc3ed87023f7b737125e31ac1f955'
 const BASE_URL = 'https://api.themoviedb.org/3'
 
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500'
